836badcb70bca7a78180bd0d6d3a3eaaa7ab2b97
RobCatton <41433657+RobCatton@users.noreply.github.com>
2025-12-03 10:57:21 +0000

    fix(NO-JIRA): Fixing test wrappings for sonar cloud complaint #patch #38

--- test/integration/narrow/routes/api-requests/application-api.test.js
+++ test/integration/narrow/routes/api-requests/application-api.test.js
@@ -15,33 +15,39 @@ describe("application api", () => {
 
   const makeLogger = () => ({ error: jest.fn() });
 
-  testWreckApiFunction({
-    fn: getApplicationsBySbi,
-    method: "get",
-    endpoint: `${config.applicationApiUri}/applications?sbi=SBI123`,
-    args: ["SBI123"],
-    returnPayload: "ABC123",
-    logger: makeLogger(),
-    outboundPayload: null,
+  test("getApplicationsBySbi", () => {
+    testWreckApiFunction({
+      fn: getApplicationsBySbi,
+      method: "get",
+      endpoint: `${config.applicationApiUri}/applications?sbi=SBI123`,
+      args: ["SBI123"],
+      returnPayload: "ABC123",
+      logger: makeLogger(),
+      outboundPayload: null,
+    });
   });
 
-  testWreckApiFunction({
-    fn: createApplication,
-    method: "post",
-    endpoint: `${config.applicationApiUri}/applications`,
-    args: [{ testData: "stuff" }],
-    outboundPayload: { testData: "stuff" },
-    returnPayload: "ABC123",
-    logger: makeLogger(),
+  test("createApplication", () => {
+    testWreckApiFunction({
+      fn: createApplication,
+      method: "post",
+      endpoint: `${config.applicationApiUri}/applications`,
+      args: [{ testData: "stuff" }],
+      outboundPayload: { testData: "stuff" },
+      returnPayload: "ABC123",
+      logger: makeLogger(),
+    });
   });
 
-  testWreckApiFunction({
-    fn: getHerds,
-    method: "get",
-    endpoint: `${config.applicationApiUri}/applications/IAHW-ABVR-1234/herds?species=beef`,
-    args: ["IAHW-ABVR-1234", "beef"],
-    outboundPayload: null,
-    returnPayload: "ABC123",
-    logger: makeLogger(),
+  test("getHerds", () => {
+    testWreckApiFunction({
+      fn: getHerds,
+      method: "get",
+      endpoint: `${config.applicationApiUri}/applications/IAHW-ABVR-1234/herds?species=beef`,
+      args: ["IAHW-ABVR-1234", "beef"],
+      outboundPayload: null,
+      returnPayload: "ABC123",
+      logger: makeLogger(),
+    });
   });
 });
