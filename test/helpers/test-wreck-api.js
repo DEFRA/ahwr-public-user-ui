456c3866888412e128062169a6de6e10feb27bd7
Rob Catton <robmcatton@gmail.com>
2025-12-03 10:51:18 +0000

    Fixing test wrappings for sonar cloud complaint

--- test/helpers/test-wreck-api.js
+++ test/helpers/test-wreck-api.js
@@ -1,6 +1,6 @@
 import Wreck from "@hapi/wreck";
 
-export function testWreckApiFunction({
+export async function testWreckApiFunction({
   fn,
   method,
   endpoint,
@@ -9,44 +9,38 @@ export function testWreckApiFunction({
   returnPayload,
   logger,
 }) {
-  describe(fn.name, () => {
-    it("returns payload when successful", async () => {
-      Wreck[method].mockResolvedValueOnce({ payload: returnPayload });
+  Wreck[method].mockResolvedValueOnce({ payload: returnPayload });
 
-      const result = await fn(...args, logger);
+  const result = await fn(...args, logger);
 
-      expect(result).toBe(returnPayload);
+  expect(result).toBe(returnPayload);
 
-      if (method === "get") {
-        expect(Wreck.get).toHaveBeenCalledWith(endpoint, { json: true });
-      } else {
-        expect(Wreck.post).toHaveBeenCalledWith(endpoint, {
-          payload: outboundPayload,
-          json: true,
-        });
-      }
-
-      expect(logger.error).not.toHaveBeenCalled();
+  if (method === "get") {
+    expect(Wreck.get).toHaveBeenCalledWith(endpoint, { json: true });
+  } else {
+    expect(Wreck.post).toHaveBeenCalledWith(endpoint, {
+      payload: outboundPayload,
+      json: true,
     });
+  }
 
-    it("logs and rethrows errors", async () => {
-      const expectedError = new Error("Whoops");
-      Wreck[method].mockImplementation(() => {
-        throw expectedError;
-      });
+  expect(logger.error).not.toHaveBeenCalled();
 
-      await expect(fn(...args, logger)).rejects.toThrow("Whoops");
-
-      if (method === "get") {
-        expect(Wreck.get).toHaveBeenCalledWith(endpoint, { json: true });
-      } else {
-        expect(Wreck.post).toHaveBeenCalledWith(endpoint, {
-          payload: outboundPayload,
-          json: true,
-        });
-      }
-
-      expect(logger.error).toHaveBeenCalledWith({ error: expectedError });
-    });
+  const expectedError = new Error("Whoops");
+  Wreck[method].mockImplementation(() => {
+    throw expectedError;
   });
+
+  await expect(fn(...args, logger)).rejects.toThrow("Whoops");
+
+  if (method === "get") {
+    expect(Wreck.get).toHaveBeenCalledWith(endpoint, { json: true });
+  } else {
+    expect(Wreck.post).toHaveBeenCalledWith(endpoint, {
+      payload: outboundPayload,
+      json: true,
+    });
+  }
+
+  expect(logger.error).toHaveBeenCalledWith({ error: expectedError });
 }
--- test/integration/narrow/routes/api-requests/claim-api.test.js
+++ test/integration/narrow/routes/api-requests/claim-api.test.js
@@ -15,33 +15,39 @@ describe("claim api", () => {
 
   const makeLogger = () => ({ error: jest.fn() });
 
-  testWreckApiFunction({
-    fn: getClaimsByApplicationReference,
-    method: "get",
-    endpoint: `${config.applicationApiUri}/applications/REF123/claims`,
-    args: ["REF123"],
-    returnPayload: "ABC123",
-    logger: makeLogger(),
-    outboundPayload: null,
+  test("getClaimsByApplicationReference", () => {
+    testWreckApiFunction({
+      fn: getClaimsByApplicationReference,
+      method: "get",
+      endpoint: `${config.applicationApiUri}/applications/REF123/claims`,
+      args: ["REF123"],
+      returnPayload: "ABC123",
+      logger: makeLogger(),
+      outboundPayload: null,
+    });
   });
 
-  testWreckApiFunction({
-    fn: submitNewClaim,
-    method: "post",
-    endpoint: `${config.applicationApiUri}/claims`,
-    args: [{ testData: "stuff" }],
-    outboundPayload: { testData: "stuff" },
-    returnPayload: "ABC123",
-    logger: makeLogger(),
+  test("submitNewClaim", () => {
+    testWreckApiFunction({
+      fn: submitNewClaim,
+      method: "post",
+      endpoint: `${config.applicationApiUri}/claims`,
+      args: [{ testData: "stuff" }],
+      outboundPayload: { testData: "stuff" },
+      returnPayload: "ABC123",
+      logger: makeLogger(),
+    });
   });
 
-  testWreckApiFunction({
-    fn: isURNUnique,
-    method: "post",
-    endpoint: `${config.applicationApiUri}/claims/is-urn-unique`,
-    args: [{ testData: "stuff" }],
-    outboundPayload: { testData: "stuff" },
-    returnPayload: "ABC123",
-    logger: makeLogger(),
+  test("isURNUnique", () => {
+    testWreckApiFunction({
+      fn: isURNUnique,
+      method: "post",
+      endpoint: `${config.applicationApiUri}/claims/is-urn-unique`,
+      args: [{ testData: "stuff" }],
+      outboundPayload: { testData: "stuff" },
+      returnPayload: "ABC123",
+      logger: makeLogger(),
+    });
   });
 });
