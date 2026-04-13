c963460820f47e16a84fc1ebbcf794ec88188e1a
Mathew Attlee <hello@codeinabox.com>
2026-04-13 16:38:08 +0100

    chore: improve error logging

--- app/cache/build-redis-client.js
+++ app/cache/build-redis-client.js
@@ -57,7 +57,7 @@ export function buildRedisClient() {
   });
 
   redisClient.on("error", (error) => {
-    getLogger().info(`Redis connection error ${error}`);
+    getLogger().error({ error, service: "redis", host }, "Redis connection error");
   });
 
   return redisClient;
