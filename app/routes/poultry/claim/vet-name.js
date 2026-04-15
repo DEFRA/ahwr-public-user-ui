230b554cb06909751d3275200041a768b80e75b4
Jorge Gueorguiev Garcia <yefo.akira@twoormore.eu>
2026-04-15 14:50:12 +0100

    Extracted common schema

--- app/routes/poultry/claim/vet-name.js
+++ app/routes/poultry/claim/vet-name.js
@@ -7,16 +7,7 @@ import {
   setSessionData,
 } from "../../../session/index.js";
 import { poultryClaimRoutes, poultryClaimViews } from "../../../constants/routes.js";
-
-const errorMessages = {
-  enterName: "Enter the vet's name",
-  nameLength: "Vet's name must be 50 characters or fewer",
-  namePattern:
-    "Vet's name must only include letters a to z, numbers and special characters such as hyphens, spaces, apostrophes, ampersands, commas, brackets or a forward slash",
-};
-
-const MAX_VET_NAME_LENGTH = 50;
-const VET_NAME_PATTERN = /^[A-Za-z0-9&,' \-/()]+$/;
+import { vetsNameSchema } from "../../utils/schemas.js";
 
 const getHandler = {
   method: "GET",
@@ -42,18 +33,7 @@ const postHandler = {
   options: {
     validate: {
       payload: Joi.object({
-        vetsName: Joi.string()
-          .trim()
-          .max(MAX_VET_NAME_LENGTH)
-          .pattern(VET_NAME_PATTERN)
-          .required()
-          .messages({
-            "any.required": errorMessages.enterName,
-            "string.base": errorMessages.enterName,
-            "string.empty": errorMessages.enterName,
-            "string.max": errorMessages.nameLength,
-            "string.pattern.base": errorMessages.namePattern,
-          }),
+        vetsName: vetsNameSchema,
       }),
       failAction: async (request, h, error) => {
         request.logger.error({ error });
