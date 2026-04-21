4832f80323568c45c6c02fde656da877e6c18f0a
Jorge Gueorguiev Garcia <yefo.akira@twoormore.eu>
2026-04-21 15:39:00 +0100

    Initial setup of the select-the-site screen

--- app/routes/poultry/claim/select-the-site.js
+++ app/routes/poultry/claim/select-the-site.js
@@ -1,12 +1,51 @@
+import { poultryClaimRoutes, poultryClaimViews } from "../../../constants/routes.js";
+import { getSessionData, sessionEntryKeys } from "../../../session/index.js";
+
+const radioValueNewSite = "NEW_SITE";
+
+const getUniqueSites = (previousClaims) => {
+  if (!previousClaims) return [];
+
+  const seen = new Set();
+  return previousClaims
+    .filter((claim) => claim.herd?.name && claim.herd?.cph)
+    .filter((claim) => {
+      const key = `${claim.herd.name}|${claim.herd.cph}`;
+      if (seen.has(key)) return false;
+      seen.add(key);
+      return true;
+    })
+    .map((claim) => ({
+      id: claim.herd.id,
+      name: claim.herd.name,
+      cph: claim.herd.cph,
+    }));
+};
+
 const getHandler = {
   method: "GET",
-  path: "/select-the-site",
-  handler: async (_request, _h) => {},
+  path: poultryClaimRoutes.selectTheSite,
+  handler: async (request, h) => {
+    const { siteSelected, previousClaims } = getSessionData(request, sessionEntryKeys.poultryClaim);
+
+    const previousSites = getUniqueSites(previousClaims);
+
+    return h.view(poultryClaimViews.selectTheSite, {
+      backLink: poultryClaimRoutes.dateOfReview,
+      pageTitleText:
+        previousSites.length > 1
+          ? `Select the site you are claiming for`
+          : `Is this the same site you have previously claimed for?`,
+      sites: previousSites,
+      radioValueNewSite,
+      siteSelected,
+    });
+  },
 };
 
 const postHandler = {
   method: "POST",
-  path: "/select-the-site",
+  path: poultryClaimRoutes.selectTheSite,
   handler: async (_request, _h) => {},
 };
 
