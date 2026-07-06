5f0d65958e000c0553863ba0767337ae0a23c44b
Jorge Gueorguiev Garcia <yefo.akira@twoormore.eu>
2026-07-06 14:11:22 +0100

    changing browser title cph

--- test/integration/narrow/routes/poultry/claim/cph.test.js
+++ test/integration/narrow/routes/poultry/claim/cph.test.js
@@ -70,15 +70,6 @@ describe("/cph tests", () => {
     jest.resetAllMocks();
   });
 
-  const expectSiteText = ($) => {
-    expect($("title").text().trim()).toContain(
-      "Enter the CPH number for this site - Get funding to improve animal health and welfare - GOV.UKGOV.UK",
-    );
-    expect($(".govuk-label").text().trim()).toBe(
-      "Enter the county parish holding (CPH) number for this site",
-    );
-  };
-
   describe("GET", () => {
     const getResponse = () => {
       when(getSessionData)
@@ -86,7 +77,7 @@ describe("/cph tests", () => {
         .mockReturnValue({ reference: "TEMP-6GSE-PIR8" });
       return server.inject({ method: "GET", url, auth });
     };
-    testBrowserPageTitle({ title: "Enter the CPH number for this site", getResponse });
+    testBrowserPageTitle({ title: "CPH number for this poultry site", getResponse });
     testPageHeading({
       heading: "Enter the county parish holding (CPH) number for this site",
       getResponse,
@@ -174,58 +165,79 @@ describe("/cph tests", () => {
     });
 
     describe("cph number validation", () => {
-      test("display errors when cph number is missing", async () => {
-        getSessionData.mockReturnValue({
-          reference: "TEMP-6GSE-PIR8",
+      describe("display errors when cph number is missing", () => {
+        const getResponse = () => {
+          getSessionData.mockReturnValue({
+            reference: "TEMP-6GSE-PIR8",
+          });
+
+          return server.inject({
+            method: "POST",
+            url,
+            auth,
+            payload: { crumb },
+            headers: { cookie: `crumb=${crumb}` },
+          });
+        };
+        testBrowserPageTitle({ title: "Error: CPH number for this poultry site", getResponse });
+        testPageHeading({
+          heading: "Enter the county parish holding (CPH) number for this site",
+          getResponse,
         });
 
-        const res = await server.inject({
-          method: "POST",
-          url,
-          auth,
-          payload: { crumb },
-          headers: { cookie: `crumb=${crumb}` },
-        });
+        test("display errors when cph number is missing", async () => {
+          const res = await getResponse();
 
-        expect(await axe(res.payload)).toHaveNoViolations();
-        const $ = cheerio.load(res.payload);
-        expect(res.statusCode).toBe(400);
-        expect($("h2.govuk-error-summary__title").text()).toContain("There is a problem");
-        expect($('a[href="#herdCph"]').text()).toContain(
-          "Enter the CPH for this site in the format 12/345/6789",
-        );
-        expect($('p[id="herdCph-error"]').text()).toContain(
-          "Enter the CPH for this site in the format 12/345/6789",
-        );
-        expectSiteText($);
-        expect(emitHerdEvent).not.toHaveBeenCalled();
+          expect(await axe(res.payload)).toHaveNoViolations();
+          const $ = cheerio.load(res.payload);
+          expect(res.statusCode).toBe(400);
+          expect($("h2.govuk-error-summary__title").text()).toContain("There is a problem");
+          expect($('a[href="#herdCph"]').text()).toContain(
+            "Enter the CPH for this site in the format 12/345/6789",
+          );
+          expect($('p[id="herdCph-error"]').text()).toContain(
+            "Enter the CPH for this site in the format 12/345/6789",
+          );
+          expect(emitHerdEvent).not.toHaveBeenCalled();
+        });
       });
 
-      test("display errors when cph number does not contain digits", async () => {
-        getSessionData.mockReturnValue({
-          reference: "TEMP-6GSE-PIR8",
+      describe("display errors when cph number does not contain digits", () => {
+        const getResponse = () => {
+          getSessionData.mockReturnValue({
+            reference: "TEMP-6GSE-PIR8",
+          });
+
+          return server.inject({
+            method: "POST",
+            url,
+            auth,
+            payload: { crumb, herdCph: "aa/222/3333" },
+            headers: { cookie: `crumb=${crumb}` },
+          });
+        };
+
+        testBrowserPageTitle({ title: "Error: CPH number for this poultry site", getResponse });
+        testPageHeading({
+          heading: "Enter the county parish holding (CPH) number for this site",
+          getResponse,
         });
 
-        const res = await server.inject({
-          method: "POST",
-          url,
-          auth,
-          payload: { crumb, herdCph: "aa/222/3333" },
-          headers: { cookie: `crumb=${crumb}` },
-        });
+        test("display errors when cph number does not contain digits", async () => {
+          const res = await getResponse();
 
-        expect(await axe(res.payload)).toHaveNoViolations();
-        const $ = cheerio.load(res.payload);
-        expect(res.statusCode).toBe(400);
-        expect($("h2.govuk-error-summary__title").text()).toContain("There is a problem");
-        expect($('a[href="#herdCph"]').text()).toContain(
-          "Enter the CPH for this site in the format 12/345/6789",
-        );
-        expect($('p[id="herdCph-error"]').text()).toContain(
-          "Enter the CPH for this site in the format 12/345/6789",
-        );
-        expectSiteText($);
-        expect(emitHerdEvent).not.toHaveBeenCalled();
+          expect(await axe(res.payload)).toHaveNoViolations();
+          const $ = cheerio.load(res.payload);
+          expect(res.statusCode).toBe(400);
+          expect($("h2.govuk-error-summary__title").text()).toContain("There is a problem");
+          expect($('a[href="#herdCph"]').text()).toContain(
+            "Enter the CPH for this site in the format 12/345/6789",
+          );
+          expect($('p[id="herdCph-error"]').text()).toContain(
+            "Enter the CPH for this site in the format 12/345/6789",
+          );
+          expect(emitHerdEvent).not.toHaveBeenCalled();
+        });
       });
 
       test("it normalizes the cph number before validation", async () => {
@@ -249,69 +261,90 @@ describe("/cph tests", () => {
         expect(emitHerdEvent).toHaveBeenCalled();
       });
 
-      test("displays error when cph has been used in other claims", async () => {
+      describe("displays error when cph has been used in other claims", () => {
+        const getResponse = () => {
+          getSessionData.mockReturnValue({
+            reference: "TEMP-6GSE-PIR8",
+            herdId: "e3d320b7-b2cf-469a-903f-ead7587d98e9",
+          });
+          getClaimsCount.mockResolvedValueOnce({ count: 2 });
+
+          return server.inject({
+            method: "POST",
+            url,
+            auth,
+            payload: { crumb, herdCph: "22/333/4444" },
+            headers: { cookie: `crumb=${crumb}` },
+          });
+        };
+        testBrowserPageTitle({ title: "Error: CPH number for this poultry site", getResponse });
+        testPageHeading({
+          heading: "Enter the county parish holding (CPH) number for this site",
+          getResponse,
+        });
+
+        test("displays error when cph has been used in other claims", async () => {
+          const res = await getResponse();
+          expect(await axe(res.payload)).toHaveNoViolations();
+          const $ = cheerio.load(res.payload);
+          expect(res.statusCode).toBe(400);
+          expect($("h2.govuk-error-summary__title").text()).toContain("There is a problem");
+          expect($('a[href="#herdCph"]').text()).toContain(
+            "Enter a CPH that you have not used for a different site",
+          );
+          expect($('p[id="herdCph-error"]').text()).toContain(
+            "Enter a CPH that you have not used for a different site",
+          );
+
+          expect(emitHerdEvent).not.toHaveBeenCalled();
+          expect(getClaimsCount).toHaveBeenCalledWith(
+            "22/333/4444",
+            "e3d320b7-b2cf-469a-903f-ead7587d98e9",
+            POULTRY_SCHEME,
+            expect.any(Object),
+          );
+        });
+      });
+    });
+
+    describe("display errors with back link to select site when payload invalid and updating an existing herd", () => {
+      const getResponse = () => {
         getSessionData.mockReturnValue({
           reference: "TEMP-6GSE-PIR8",
-          herdId: "e3d320b7-b2cf-469a-903f-ead7587d98e9",
+          herdVersion: 2,
         });
-        getClaimsCount.mockResolvedValueOnce({ count: 2 });
 
-        const res = await server.inject({
+        return server.inject({
           method: "POST",
           url,
           auth,
-          payload: { crumb, herdCph: "22/333/4444" },
+          payload: { crumb },
           headers: { cookie: `crumb=${crumb}` },
         });
+      };
 
+      testBrowserPageTitle({ title: "Error: CPH number for this poultry site", getResponse });
+      testPageHeading({
+        heading: "Enter the county parish holding (CPH) number for this site",
+        getResponse,
+      });
+
+      test("display errors with back link to select site when payload invalid and updating an existing herd", async () => {
+        const res = await getResponse();
         expect(await axe(res.payload)).toHaveNoViolations();
         const $ = cheerio.load(res.payload);
         expect(res.statusCode).toBe(400);
         expect($("h2.govuk-error-summary__title").text()).toContain("There is a problem");
         expect($('a[href="#herdCph"]').text()).toContain(
-          "Enter a CPH that you have not used for a different site",
+          "Enter the CPH for this site in the format 12/345/6789",
         );
         expect($('p[id="herdCph-error"]').text()).toContain(
-          "Enter a CPH that you have not used for a different site",
+          "Enter the CPH for this site in the format 12/345/6789",
         );
-        expectSiteText($);
+
+        expect($(".govuk-back-link").attr("href")).toContain("/select-site");
         expect(emitHerdEvent).not.toHaveBeenCalled();
-        expect(getClaimsCount).toHaveBeenCalledWith(
-          "22/333/4444",
-          "e3d320b7-b2cf-469a-903f-ead7587d98e9",
-          POULTRY_SCHEME,
-          expect.any(Object),
-        );
       });
-    });
-
-    test("display errors with back link to select site when payload invalid and updating an existing herd", async () => {
-      getSessionData.mockReturnValue({
-        reference: "TEMP-6GSE-PIR8",
-        herdVersion: 2,
-      });
-
-      const res = await server.inject({
-        method: "POST",
-        url,
-        auth,
-        payload: { crumb },
-        headers: { cookie: `crumb=${crumb}` },
-      });
-
-      expect(await axe(res.payload)).toHaveNoViolations();
-      const $ = cheerio.load(res.payload);
-      expect(res.statusCode).toBe(400);
-      expect($("h2.govuk-error-summary__title").text()).toContain("There is a problem");
-      expect($('a[href="#herdCph"]').text()).toContain(
-        "Enter the CPH for this site in the format 12/345/6789",
-      );
-      expect($('p[id="herdCph-error"]').text()).toContain(
-        "Enter the CPH for this site in the format 12/345/6789",
-      );
-      expectSiteText($);
-      expect($(".govuk-back-link").attr("href")).toContain("/select-site");
-      expect(emitHerdEvent).not.toHaveBeenCalled();
     });
   });
 });
