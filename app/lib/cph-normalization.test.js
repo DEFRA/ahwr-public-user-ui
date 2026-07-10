d79fbc663032f75fc15252cf38ba27becce11e5d
Jorge Gueorguiev Garcia <yefo.akira@twoormore.eu>
2026-07-10 12:03:10 +0100

    parameterization of tests on cph-normalization

--- app/lib/cph-normalization.test.js
+++ app/lib/cph-normalization.test.js
@@ -1,74 +1,18 @@
 import { normalizeCphNumber } from "./cph-normalization.js";
 
 describe("Normalization of cph number", () => {
-  it("returns undefined unchanged", () => {
-    const actual = normalizeCphNumber(undefined);
-
-    const expected = undefined;
-
-    expect(actual).toBe(expected);
-  });
-
-  it("returns null unchanged", () => {
-    const actual = normalizeCphNumber(null);
-
-    const expected = null;
-
-    expect(actual).toBe(expected);
-  });
-
-  it("returns proper number unchanged", () => {
-    const actual = normalizeCphNumber("12/123/1234");
-
-    const expected = "12/123/1234";
-
-    expect(actual).toBe(expected);
-  });
-
-  it("strips spaces at the beginning", () => {
-    const actual = normalizeCphNumber(" 12/123/1234");
-
-    const expected = "12/123/1234";
-
-    expect(actual).toBe(expected);
-  });
-
-  it("strips spaces at the end", () => {
-    const actual = normalizeCphNumber("12/123/1234 ");
-
-    const expected = "12/123/1234";
-
-    expect(actual).toBe(expected);
-  });
-
-  it("strips spaces in the middle", () => {
-    const actual = normalizeCphNumber("12 / 12   3/1 2 3 4");
-
-    const expected = "12/123/1234";
-
-    expect(actual).toBe(expected);
-  });
-
-  it("adds delimiters if none present", () => {
-    const actual = normalizeCphNumber("121231234");
-
-    const expected = "12/123/1234";
-
-    expect(actual).toBe(expected);
-  });
-
-  it("normalize delimiters if present", () => {
-    const actual = normalizeCphNumber("12-123-1234");
-
-    const expected = "12/123/1234";
-
-    expect(actual).toBe(expected);
-  });
-
-  it("returns original if delimiters are not matched", () => {
-    const actual = normalizeCphNumber(" 12-1 23%1234");
-
-    const expected = " 12-1 23%1234";
+  it.each([
+    ["returns undefined unchanged", undefined, undefined],
+    ["returns null unchanged", null, null],
+    ["returns proper number unchanged", "12/123/1234", "12/123/1234"],
+    ["strips spaces at the beginning", " 12/123/1234", "12/123/1234"],
+    ["strips spaces at the end", "12/123/1234 ", "12/123/1234"],
+    ["strips spaces in the middle", "12 / 12   3/1 2 3 4", "12/123/1234"],
+    ["adds delimiters if none present", "121231234", "12/123/1234"],
+    ["normalize delimiters if present", "12-123-1234", "12/123/1234"],
+    ["returns original if delimiters are not matched", " 12-1 23%1234", " 12-1 23%1234"],
+  ])("%s", (_name, input, expected) => {
+    const actual = normalizeCphNumber(input);
 
     expect(actual).toBe(expected);
   });
