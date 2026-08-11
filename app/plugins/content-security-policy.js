1c820e562b55d8154000fc2fe8a8b255a191e75a
Duncan Sommerville <233909619+sommerville-ee-defra@users.noreply.github.com>
2026-08-11 13:42:37 +0100

    fix(security): (AHWR-2148) allow the Defra ID origin as a form-action target #patch

--- app/plugins/content-security-policy.js
+++ app/plugins/content-security-policy.js
@@ -1,7 +1,12 @@
 import Blankie from "blankie";
+import { authConfig } from "../config/auth.js";
 
 const GOOGLE_ANALYTICS = "*.google-analytics.com";
 const GOOGLE_TAG_MANAGER = "*.googletagmanager.com";
+
+// Session expiry redirects form submissions on to Defra ID, and form-action applies to the whole redirect chain
+export const buildFormActionSources = ({ tenantName, hostname }) =>
+  tenantName ? ["self", new URL(hostname).origin] : ["self"];
 
 export const contentSecurityPolicyPlugin = {
   plugin: Blankie,
@@ -9,7 +14,7 @@ export const contentSecurityPolicyPlugin = {
     defaultSrc: ["self"],
     objectSrc: ["none"],
     scriptSrc: ["self", "www.google-analytics.com", GOOGLE_TAG_MANAGER],
-    formAction: ["self"],
+    formAction: buildFormActionSources(authConfig.defraId),
     baseUri: ["self"],
     connectSrc: ["self", GOOGLE_ANALYTICS, "*.analytics.google.com", GOOGLE_TAG_MANAGER],
     styleSrc: ["self", "tagmanager.google.com", "*.googleapis.com"],
