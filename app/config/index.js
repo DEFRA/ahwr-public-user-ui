229a651788ecd055e040ac661156f423e2121934
Mathew Attlee <hello@codeinabox.com>
2026-08-12 12:43:10 +0100

    refactor: rename to sessionTimeoutInMs

--- app/config/index.js
+++ app/config/index.js
@@ -10,7 +10,7 @@ const HOURS_IN_DAY = 24;
 const DAYS_IN_YEAR = 365;
 const MS_IN_SECOND = 1000;
 const THIRTY = 30;
-const thirtyMinutesInMs = MS_IN_SECOND * SECONDS_IN_MINUTE * THIRTY;
+const sessionTimeoutInMs = MS_IN_SECOND * SECONDS_IN_MINUTE * THIRTY; // 30 minutes
 const oneYearInMs = MS_IN_SECOND * SECONDS_IN_HOUR * HOURS_IN_DAY * DAYS_IN_YEAR;
 const defaultApiKey = "c19fcb0d-a6d2-4d9e-9325-16d44ddc0724";
 
@@ -114,7 +114,7 @@ export const getConfig = () => {
   const builtConfig = {
     namespace: process.env.NAMESPACE,
     cache: {
-      expiresIn: thirtyMinutesInMs,
+      expiresIn: sessionTimeoutInMs,
       name: "session",
       options: {
         host: process.env.REDIS_HOST || "redis-hostname.default",
@@ -135,7 +135,7 @@ export const getConfig = () => {
       isSameSite: process.env.DISABLE_COOKIE_SAME_SITE === "true" ? false : "Lax",
       isSecure: process.env.NODE_ENV === "production",
       password: process.env.COOKIE_PASSWORD,
-      ttl: thirtyMinutesInMs,
+      ttl: sessionTimeoutInMs,
     },
     cookiePolicy: {
       clearInvalid: false,
